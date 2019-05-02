64e17b63a608e4cf38d6ff6dbcd075bc89160aec
aquibqureshi26 <40310215+aquibqureshi26@users.noreply.github.com>
2019-05-02 11:34:03 +0530

    Add files via upload

---
+++ Copy-FileFromAzure.ps1
@@ -0,0 +1,15 @@
+﻿
+$dest = "C:\downloadpackage"
+$source = "https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/windows/amd64/kubectl.exe"
+New-Item -Path $dest -ItemType directory
+Invoke-WebRequest $source -OutFile "$dest\kubectl.exe"
+$env:Path += ";$dest"
+
+#Install-WindowsFeature -Name Microsoft-Windows-Subsystem-Linux
+
+$docker = "https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe"
+
+Invoke-WebRequest $docker -OutFile "$dest\docker.exe"
+
+
+
